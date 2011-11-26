e3d8ae276a828f995670d19cb2878f28c7f916c5
Raedism <shorty626fl@gmail.com>
2011-11-25 21:52:52 -0700

    Fixed file read, for cross compatibility.

--- ObjectHandler.java
+++ ObjectHandler.java
@@ -46,7 +46,7 @@ public class ObjectHandler {
 			ObjectOpen[i] = false;
 			ResetFire(i);
 		}
-		loadObjects("config\\objects.cfg");
+		loadObjects(".//config//objects.cfg");
 	}
 
 	public void process() {
